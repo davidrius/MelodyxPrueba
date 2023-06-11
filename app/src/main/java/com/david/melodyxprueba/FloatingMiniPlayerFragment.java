e1efa64d3ef81c0e818b58d91e0beb90fa3978b3
davidrius <davidmnkf@gmail.com>
2023-06-12 00:09:32 +0200

    Prueba1

--- app/src/main/java/com/david/melodyxprueba/FloatingMiniPlayerFragment.java
+++ app/src/main/java/com/david/melodyxprueba/FloatingMiniPlayerFragment.java
@@ -62,7 +62,6 @@ public class FloatingMiniPlayerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.mini_player, container, false);
     }
 
--- app/src/main/java/com/david/melodyxprueba/MainActivity.java
+++ app/src/main/java/com/david/melodyxprueba/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Splash Screen
+
+        try {
+            Thread.sleep(2000);
+
+            setTheme(R.style.Theme_MelodyxPrueba);
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
 
         final Fragment fragmentArtistaPrueba = new FragmentArtistaPrueba();
 
